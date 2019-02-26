f1cf0f53ddb576df1f2b046271aa617f7a1bad06
degalo93 <denisgalo10@gmail.com>
2019-02-26 15:54:26 -0800

    added more style and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,57 @@
-var comtarget = 52;
+$(document).ready(function() {
 
-$("#comtarget-text").text(comtarget);
+    //Randomize a number from 20 to 52
+    var comtarget = Math.floor((Math.random() * 20) + 42);
 
-var num = 0;
+    $("#comtarget-text").text(comtarget);
 
-var numberOpt = [8, 2, 3, 7];
+    var num = 0;
+    var winsCounter = 0;
+    var lossCounter = 0;
 
-for (var i = 0; i < numberOpt.length; i++) {
-    var imageCards = $("<img>");
-    imageCards.addClass("cards-images");
+    var numberOpt = [8, 2, 3, 7];
 
-    if (i === 0) {
-        imageCards.attr("src", "assets/images/clubs.jpg");
-    } else if (i === 1) {
-        imageCards.attr("src", "assets/images/acespade.jpg");
-    } else if (i === 2) {
-        imageCards.attr("src", "assets/images/hearts.jpg");
-    } else if (i === 3) {
-        imageCards.attr("src", "assets/images/diamond.jpg");
+    for (var i = 0; i < numberOpt.length; i++) {
+        var imageCards = $("<img>");
+        imageCards.addClass("cards-images");
+
+        if (i === 0) {
+            imageCards.attr("src", "assets/images/clubs.jpg");
+        } else if (i === 1) {
+            imageCards.attr("src", "assets/images/acespade.jpg");
+        } else if (i === 2) {
+            imageCards.attr("src", "assets/images/hearts.jpg");
+        } else if (i === 3) {
+            imageCards.attr("src", "assets/images/diamond.jpg");
+        }
+        imageCards.attr("data-cardvalue", numberOpt[i]);
+        $("#cards").append(imageCards);
+
     }
-    imageCards.attr("data-cardvalue", numberOpt[i]);
-    $("#cards").append(imageCards);
 
-}
 
-$(".cards-images").on("click", function() {
 
-    var cardValue = ($(this).attr("data-cardvalue"));
-    cardValue = parseInt(cardValue);
-    num += cardValue;
-    $("#num").html("Your Score: " + num);
-    if (num === comtarget) {
-        $("#win").html("You have won!!");
-    } else if (num >= comtarget) {
-        $("#loss").html("You have lost! ");
-    }
+    $(".cards-images").on("click", function() {
+
+        var cardValue = ($(this).attr("data-cardvalue"));
+        cardValue = parseInt(cardValue);
+        num += cardValue;
+
+        $("#num").text("Your Score: " + num);
+        if (num === comtarget) {
+            $("#win").text("You have won!!");
+            winsCounter++;
+            $("#totalWins").text("Wins: " + winsCounter);
+            num = 0;
+
+        } else if (num >= comtarget) {
+            $("#loss").text("You have lost! ");
+            lossCounter++;
+            $("#totalLoss").text("Losses: " + lossCounter)
+            num = 0;
+        }
+
+
+
+    });
 });
